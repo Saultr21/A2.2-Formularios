54ffeec888b96f821bf0d50e38bdbd1cb9028360
Saultr21 <saultomastrujillorodriguez@alumno.ieselrincon.es>
2023-10-03 12:04:07 +0100

    form

--- recibe_datos.php
+++ recibe_datos.php
@@ -1,4 +1,12 @@
 <?php
 
 //Fichero recibe datos y valida
+
+//Recogemos los datos del formulario
+$nombre = $_GET['nombre'];
+$apellidos = $_GET['apellidos'];
+$email = $_GET['email'];
+$password = $_GET['password'];
+$errores[] = "";
+
 ?>
