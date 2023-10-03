c933bc03c29691c60a96f62929e61cfef565bb84
Ian Santana <iansantiagosantanaangulo@alumno.ieselrincon.es>
2023-10-03 12:06:15 +0100

    dia1ian

--- recibe_datos.php
+++ recibe_datos.php
@@ -1,4 +1,26 @@
 <?php
 
 //Fichero recibe datos y valida
+$sexo = $_POST['sexo'];
+$edad = $_POST['edad'];
+$pais = $_POST['pais'];
+$asignaturas = [];
+
+function asignaturas(){
+    if(isset($_POST['dsw'])){
+        array_push($asignaturas, "dsw");
+    }
+    if(isset($_POST['daw'])){
+        array_push($asignaturas, "daw");
+    }
+    if(isset($_POST['fol'])){
+        array_push($asignaturas, "fol");
+    }
+    if(isset($_POST['dpl'])){
+        array_push($asignaturas, "dpl");
+    }
+    if(isset($_POST['dor'])){
+        array_push($asignaturas, "dor");
+    }
+}
 ?>
